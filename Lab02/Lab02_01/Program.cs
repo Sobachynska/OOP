8fe7c1cca7b701c909ebee49da4aba7d2aca264c
Sobachynska <kn1b21.sobachynska@kpnu.edu.ua>
2023-04-07 16:49:05 +0300

    Lab02

--- Lab02/Lab02_01/Program.cs
+++ Lab02/Lab02_01/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 
-namespace MaxPositiveValue
+namespace MaxPositiveValue //максимальне позитивне значення
 {
     static class ArrayExtensions
+    
     {
         public static int[] ReplacePositiveWithMax(this int[] source)
+        //ReplacePositiveWithMax - змінити позитивне на максимальне, метод розширення для int[]типу
+        //за допомогою статичного класу ArrayExtensions
         {
-            int max = source.Max();
-            for (int i = 0; i < source.Length; i++)
+            int max = source.Max(); 
+            for (int i = 0; i < source.Length; i++) //цикл
             {
                 if (source[i] > 0)
                 {
@@ -24,7 +27,7 @@ namespace MaxPositiveValue
         {
             int[] source = { 5, 2, -7, 1, 9 };
             int[] destination = source.ReplacePositiveWithMax();
-            Console.WriteLine("Source array: [{0}]", string.Join(", ", source));
+            Console.WriteLine("Source array: [{0}]", string.Join(", ", source)); //string.Join() - метод
             Console.WriteLine("Destination array: [{0}]", string.Join(", ", destination));
         }
     }
