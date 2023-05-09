22abf9ed5772864398dc4cd223f61e3057b36414
Sobachynska <kn1b21.sobachynska@kpnu.edu.ua>
2023-05-09 20:54:24 +0300

    Lab04

---
+++ Lab04/Lab04_01/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+
+public class MyDirectory
+{
+    public string Path { get; private set; }
+
+    public MyDirectory(string path)
+    {
+        Path = path;
+    }
+
+    public void Create()
+    {
+        Directory.CreateDirectory(Path);
+    }
+
+    public void Delete()
+    {
+        Directory.Delete(Path, true);
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is MyDirectory directory &&
+               Path == directory.Path;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Path);
+    }
+
+    public override string ToString()
+    {
+        return $"Directory Path: {Path}";
+    }
+}
+
+public class MyFile
+{
+    public string Name { get; private set; }
+    public MyDirectory Directory { get; private set; }
+
+    public MyFile(string name, MyDirectory directory)
+    {
+        Name = name;
+        Directory = directory;
+    }
+
+    public void Rename(string newName)
+    {
+        var oldPath = System.IO.Path.Combine(Directory.Path, Name);
+        var newPath = System.IO.Path.Combine(Directory.Path, newName);
+        System.IO.File.Move(oldPath, newPath);
+        Name = newName;
+    }
+
+    public void Delete()
+    {
+        var path = System.IO.Path.Combine(Directory.Path, Name);
+        System.IO.File.Delete(path);
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is MyFile file &&
+               Name == file.Name &&
+               EqualityComparer<MyDirectory>.Default.Equals(Directory, file.Directory);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Directory);
+    }
+
+    public override string ToString()
+    {
+        return $"File: {Name}, Directory: {Directory}";
+    }
+}
+
+public class TextFile : MyFile
+{
+    public TextFile(string name, MyDirectory directory) : base(name, directory) { }
+
+    public void Create()
+    {
+        var path = System.IO.Path.Combine(Directory.Path, Name);
+        using (StreamWriter sw = System.IO.File.CreateText(path))
+        {
+            sw.WriteLine("");
+        }
+    }
+
+    public void AppendText(string text)
+    {
+        var path = System.IO.Path.Combine(Directory.Path, Name);
+        using (StreamWriter sw = System.IO.File.AppendText(path))
+        {
+            sw.WriteLine(text);
+        }
+    }
+
+    public void DisplayContents()
+    {
+        var path = System.IO.Path.Combine(Directory.Path, Name);
+        using (StreamReader sr = System.IO.File.OpenText(path))
+        {
+            string s;
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        MyDirectory dir = new MyDirectory("./mydir");
+        dir.Create();
+
+        TextFile file = new TextFile("mytextfile.txt", dir);
+        file.Create();
+        file.AppendText("Hello, World!");
+        file.DisplayContents();
+        file.Rename("newtextfile.txt");
+        file.DisplayContents();
+
+        dir.Delete();
+    }
+}
---
+++ Lab04/Lab04_02/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class TTriad
+{
+    protected int[] Numbers = new int[3];
+
+    public abstract void Increment();
+    public abstract void Decrement();
+
+    public override string ToString()
+    {
+        return $"{Numbers[0]}.{Numbers[1]}.{Numbers[2]}";
+    }
+}
+
+public class TDate : TTriad
+{
+    public TDate(int day, int month, int year)
+    {
+        Numbers[0] = day;
+        Numbers[1] = month;
+        Numbers[2] = year;
+    }
+
+    public override void Increment()
+    {
+        // Logic to increment date
+    }
+
+    public override void Decrement()
+    {
+        if (--Numbers[0] < 1)
+        {
+            Numbers[0] = 31;
+            if (--Numbers[1] < 1)
+            {
+                Numbers[1] = 12;
+                --Numbers[2];
+            }
+        }
+    }
+
+    public bool IsValidTime()
+    {
+        // Logic to check if date is valid time
+        return Numbers[0] <= 24 && Numbers[1] <= 60 && Numbers[2] <= 60;
+    }
+}
+
+public class TTime : TTriad
+{
+    public TTime(int hour, int minute, int second)
+    {
+        Numbers[0] = hour;
+        Numbers[1] = minute;
+        Numbers[2] = second;
+    }
+
+    public override void Increment()
+    {
+        // Logic to increment time
+    }
+
+    public override void Decrement()
+    {
+        // Logic to decrement time
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+        List<TDate> dates = new List<TDate>();
+        List<TTime> times = new List<TTime>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            dates.Add(new TDate(rand.Next(1, 31), rand.Next(1, 13), rand.Next(2000, 2023)));
+            times.Add(new TTime(rand.Next(0, 24), rand.Next(0, 60), rand.Next(0, 60)));
+        }
+
+        foreach (var date in dates)
+        {
+            Console.WriteLine("Оригiнальна дата: " + date.ToString());
+            if (!date.IsValidTime())
+            {
+                date.Decrement();
+                Console.WriteLine("Скоригована дата: " + date.ToString());
+            }
+        }
+    }
+}
