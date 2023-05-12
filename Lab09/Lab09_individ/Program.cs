de259892cd023f3ec9c7ca06fba78c57cb9f5482
Sobachynska <kn1b21.sobachynska@kpnu.edu.ua>
2023-05-12 19:45:54 +0300

    oop

---
+++ Lab09/Lab09_individ/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace EquationSolver
+{
+    public delegate double FunctionDelegate(double x);
+
+    public class ChordMethodSolver
+    {
+        private const double Epsilon = 1e-6; // Точність обчислень
+
+        public double Solve(FunctionDelegate function, double a, double b)
+        {
+            double fa = function(a);
+            double fb = function(b);
+
+            if (Math.Abs(fa) < Epsilon)
+            {
+                return a; // Значення a вже є коренем
+            }
+
+            if (Math.Abs(fb) < Epsilon)
+            {
+                return b; // Значення b вже є коренем
+            }
+
+            if (fa * fb > 0)
+            {
+                throw new ArgumentException("The function values at a and b must have opposite signs.");
+            }
+
+            double x;
+            do
+            {
+                x = (a * fb - b * fa) / (fb - fa);
+                double fx = function(x);
+
+                if (Math.Abs(fx) < Epsilon)
+                {
+                    return x; // Знайдений корінь
+                }
+
+                if (fa * fx < 0)
+                {
+                    b = x;
+                    fb = fx;
+                }
+                else
+                {
+                    a = x;
+                    fa = fx;
+                }
+            } while (Math.Abs(b - a) > Epsilon);
+
+            return (a + b) / 2; // Повертаємо середнє значення, як наближений корінь
+        }
+    }
+
+    class Program
+    {
+        static double MyFunction(double x)
+        {
+            // Задайте свою функцію тут
+            return x * x - 4;
+        }
+
+        static void Main(string[] args)
+        {
+            ChordMethodSolver solver = new ChordMethodSolver();
+            FunctionDelegate function = MyFunction;
+
+            double root = solver.Solve(function, 1, 3);
+            Console.WriteLine("Root: " + root);
+        }
+    }
+}
---
+++ Lab10/Lab10_01/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CharSet
+{
+    private HashSet<char> set;
+
+    public CharSet()
+    {
+        set = new HashSet<char>();
+    }
+
+    public CharSet(params char[] elements)
+    {
+        set = new HashSet<char>(elements);
+    }
+
+    public bool Contains(char element)
+    {
+        return set.Contains(element);
+    }
+
+    public static CharSet operator *(CharSet set1, CharSet set2)
+    {
+        CharSet result = new CharSet();
+        foreach (char element in set1.set)
+        {
+            if (set2.Contains(element))
+                result.set.Add(element);
+        }
+        return result;
+    }
+
+    public static bool operator >(CharSet set1, CharSet set2)
+    {
+        foreach (char element in set2.set)
+        {
+            if (!set1.Contains(element))
+                return false;
+        }
+        return true;
+    }
+
+    public static bool operator <(CharSet set1, CharSet set2)
+    {
+        return set2 > set1;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CharSet set1 = new CharSet('a', 'b', 'c');
+        CharSet set2 = new CharSet('b', 'c', 'd');
+
+        Console.WriteLine("Set 1: " + string.Join(", ", set1));
+        Console.WriteLine("Set 2: " + string.Join(", ", set2));
+        Console.WriteLine();
+
+        Console.WriteLine("Set 1 contains 'a': " + set1.Contains('a'));
+        Console.WriteLine("Set 2 contains 'd': " + set2.Contains('d'));
+        Console.WriteLine();
+
+        CharSet intersection = set1 * set2;
+        Console.WriteLine("Intersection: " + string.Join(", ", intersection));
+        Console.WriteLine();
+
+        Console.WriteLine("Set 1 is a subset of Set 2: " + (set1 < set2));
+        Console.WriteLine("Set 2 is a subset of Set 1: " + (set2 < set1));
+    }
+}
